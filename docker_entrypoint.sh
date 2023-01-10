8bdbc487d61a785b8609916bb9bc07c7883604ca
Sky King <gkelley@slmail.us>
2023-01-10 16:26:10 -0500

    Update docker_entrypoint.sh

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,13 +2,17 @@
 
 if [ -e ~/.epic/main/bs ]
 then
+	echo "Skiping"
 else
 	echo "Setting up bootstrap chain_data file..."
+	mkdir -p ~/.epic
+	mkdir -p ~/.epic/main
 	wget https://epiccash.s3.sa-east-1.amazonaws.com/mainnet.zip -P ~/.epic/main
 	unzip ~/.epic/main/mainnet.zip -d ~/.epic/main
 	rm ~/.epic/main/mainnet.zip
 	touch ~/.epic/main/bs
+	cp /epic/* ~/.epic/main
 	echo "Done"
 fi
 
-exec tini epic-node
+exec /tini epic-node
