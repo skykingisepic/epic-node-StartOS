4520e3740e051d79f751eb9aefd8b8c5a10f95ee
Sky King <gkelley@slmail.us>
2023-09-06 16:33:51 -0400

    Add files via upload

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -e ~/.epic/main/bs ]
 then
@@ -7,9 +7,9 @@ else
 	echo "Setting up bootstrap chain_data file..."
 	mkdir -p ~/.epic
 	mkdir -p ~/.epic/main
-	wget https://epiccash.s3.sa-east-1.amazonaws.com/mainnet.zip -P ~/.epic/main
-	unzip ~/.epic/main/mainnet.zip -d ~/.epic/main
-	rm ~/.epic/main/mainnet.zip
+	wget https://bootstrap.epic.tech/bootstrap.zip -P ~/.epic/main
+	unzip ~/.epic/main/bootstrap.zip -d ~/.epic/main
+	rm ~/.epic/main/bootstrap.zip
 	touch ~/.epic/main/bs
 	cp /epic/* ~/.epic/main
 	echo "Done"
