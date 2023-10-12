9a0a2b294074361ed0ca0faf37f1870b4263926f
Sky King <gkelley@slmail.us>
2023-10-12 18:54:04 -0400

    Add files via upload

--- checkup.sh
+++ checkup.sh
@@ -5,7 +5,8 @@ if (($DURATION <= 6000 )); then
     exit 60
 fi
 
-result=$(curl -s -H 'content-type: text/plain;' http://localhost:3413/v1/status | grep no_sync)
+sync=$(curl -s -H 'content-type: text/plain;' http://localhost:3413/v1/status | grep sync_status)
+ht=$(curl -s -H 'content-type: text/plain;' http://localhost:3413/v1/status | grep height)
 
 error_code=$?
 if [ $error_code -ne 0 ]; then
@@ -13,14 +14,17 @@ if [ $error_code -ne 0 ]; then
     exit $error_code
 fi
 
-if [[ "$result" == *"no_sync"* ]]; then
+if [[ "$sync" == *"no_sync"* ]]; then
   echo "Node Synced and Running..." >&2
+  echo "Blockchain height:" ${ht:14:7} >&2
   exit 61
-elif [[ "$result" == *"awaiting"* ]]; then
+elif [[ "$sync" == *"awaiting"*  || "$sync" == *"body_sync"* || "$sync" == *"header_sync"* ]]; then
   echo "Node is Syncing..." >&2
+  echo "Blockchain height:" ${ht:14} >&2
   exit 61
 else
   echo "Check Node Status via ssh" >&2
   exit 61
 fi
+
 
